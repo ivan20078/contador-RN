d558a91cab723d921fc40578bb1871c8f339bd97
ivan20078 <ivan20078261@gmail.com>
2024-10-09 13:43:29 -0300

    first commit

--- App.js
+++ App.js
@@ -1,11 +1,30 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 
 export default function App() {
+  const [cont, setCont] = useState(0);
+  const restar = () => {
+    setCont(prevcont => prevcont - 1);
+  };
+  const sumar = () => {
+    setCont(prevcont => prevcont + 1);
+  };
+
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.counterContainer}>
+        <Text style={styles.cont}>Contador</Text>
+        <Text style={styles.cero}>{cont}</Text>
+      </View>
+      <View style={styles.buttonContainer} >
+        <TouchableOpacity onPress={restar} style={styles.botonesLeft}>
+          <Text style={styles.textos}>-</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.botonesRight}onPress={sumar}>
+          <Text style={styles.textos}>+</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
@@ -15,6 +34,57 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'space-between',
+  },
+  counterContainer: {
+    alignItems: 'center',
+    marginTop: 50,
+  },
+  cont:{
+    fontWeight:"bold",
+    fontSize:40,
+  },
+  cero: {
+    fontWeight:"bold",
+    fontSize: 90,
+    textAlign: "center",
+    marginTop:50,
+  },
+  textos: {
+    fontSize: 34,
+    color: "#fff",
+  },
+  buttonContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'flex-end', // Alinea los botones en el eje vertical
+  },
+  botonesLeft: {
+    height:60,
+    width:40,
+    flex: 1,
+    backgroundColor: "#af7ac5",
+    paddingLeft: 10,
+    marginRight: 10,
+    textAlign: "center",
+    alignItems: 'center',
+    justifyContent:"center",
+    borderTopRightRadius:20,
+    borderBottomRightRadius:40,
+    borderTopLeftRadius:30,
+  },
+  botonesRight: {
+    height:60,
+    width:40,
+    flex: 1,
+    backgroundColor: "#af7ac5",
+    paddingLeft: 10,
+    marginLeft: 10,
+    textAlign: "center",
+    alignItems: 'center',
+    justifyContent:"center",
+    borderTopLeftRadius:20,
+    borderBottomLeftRadius:40,
+    borderTopRightRadius:30,
   },
 });
